78c8fc2770ac0f65c14493f2c26a3cda31a9a10b
Cu lam <cu.lam@qdc.local>
2021-06-19 16:33:17 +0700

    Wishlist done

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -62,16 +62,16 @@
                                 @csrf
                                 <input type="hidden" value="{{ $pro->product_id }}"
                                     class="cart_product_id_{{ $pro->product_id }}">
-                                <input type="hidden" value="{{ $pro->product_name }}"
+                                <input type="hidden" id="wishlist_productname{{$pro->product_id}}" value="{{ $pro->product_name }}"
                                     class="cart_product_name_{{ $pro->product_id }}">
                                 <input type="hidden" value="{{ $pro->product_image }}"
                                     class="cart_product_image_{{ $pro->product_id }}">
-                                <input type="hidden" value="{{ $pro->product_price }}"
+                                <input type="hidden" id="wishlist_productprice{{$pro->product_id}}" value="{{ $pro->product_price }}"
                                     class="cart_product_price_{{ $pro->product_id }}">
                                 <input type="hidden" value="1" class="cart_product_qty_{{ $pro->product_id }}">
 
-                                <a href="{{ URL::to('details-product/' . $pro->product_slug) }}">
-                                    <img src="{{ URL::to('upload/product/' . $pro->product_image) }}" alt="" />
+                                <a id="wishlist_producturl{{$pro->product_id}}" href="{{ URL::to('details-product/' . $pro->product_slug) }}">
+                                    <img id="wishlist_productimage{{$pro->product_id}}" src="{{ URL::to('upload/product/' . $pro->product_image) }}" alt="" />
                                     <h2>{{ number_format($pro->product_price) . ' ' . 'VND' }}</h2>
                                     <p>{{ $pro->product_name }}</p>
                                 </a>
@@ -89,7 +89,9 @@
                     </div>
                     <div class="choose">
                         <ul class="nav nav-pills nav-justified">
-                            <li><a href="#"><i class="fa fa-plus-square"></i>Yêu thích</a></li>
+                            <li><i class="fa fa-plus-square"></i>
+                                <button class="button_wishlist" id="{{$pro->product_id}}" onclick="add_wistlist(this.id);"><span>Yêu thích</span></button>
+                            </li>
                             <li><a href="#"><i class="fa fa-plus-square"></i>So sánh</a></li>
                         </ul>
                     </div>
